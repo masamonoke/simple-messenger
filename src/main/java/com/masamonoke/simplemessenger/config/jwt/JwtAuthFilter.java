a89c7b42bf24dad97d41ef345ee08a5f05084632
masamonoke <m.rudakov@yahoo.com>
2023-10-08 16:21:20 +0300

    refresh token

--- src/main/java/com/masamonoke/simplemessenger/config/jwt/JwtAuthFilter.java
+++ src/main/java/com/masamonoke/simplemessenger/config/jwt/JwtAuthFilter.java
@@ -1,5 +1,6 @@
 package com.masamonoke.simplemessenger.config.jwt;
 
+import com.masamonoke.simplemessenger.repo.TokenRepo;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +20,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private final TokenRepo tokenRepo;
     private final UserDetailsService userDetailsService;
 
     @Override
@@ -36,7 +38,10 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         var username = jwtService.extractUsername(jwt);
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             var userDetails = userDetailsService.loadUserByUsername(username);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
+            var isTokenValid = tokenRepo.findByToken(jwt)
+                    .map(t -> !t.isExpired() && !t.isRevoked())
+                    .orElse(false);
+            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
                 var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
